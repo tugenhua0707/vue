67c8a1ebd684ea9c6ab4adecea6c8e3af067b361
tugenhua <tugenhua@tugenhuadeMacBook-Pro.local>
2017-10-27 23:47:17 +0800

    update

---
+++ slot/js/scope.js
@@ -0,0 +1,24 @@
+
+Vue.component('tb-list', {
+  template: '#tb-list',
+  props: {
+    data: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+  beforeMount() {
+
+  },
+  mounted() {
+
+  },
+  methods: {
+    
+  }
+})
